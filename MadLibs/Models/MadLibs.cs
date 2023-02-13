d77f62d04be06d523d688713427d86b2abbd582b
Chloe ONeil <chloesporri@yahoo.com>
2023-02-12 16:28:56 -0800

    add MadLibs.cs with getters and setters Co-authored-by: Najma Abdi <anajmabdi@gmail.com>

---
+++ MadLibs/Models/MadLibs.cs
@@ -0,0 +1,27 @@
+namespace MadLibs.Models 
+{
+    public class EntryForm
+    {
+        public string Noun1 { get; set; }
+
+        public string Noun2 { get; set; }
+
+        public string Noun3 { get; set; }
+
+        public string Adjective1 { get; set; }
+
+        public string Adjective2 { get; set; }
+
+        public string Food1 { get; set; }
+
+        public string Food2 { get; set; }
+
+        public string Verb { get; set; }
+
+        public string Verbing { get; set; }
+
+        public string Color1 { get; set; }
+
+        public string Color2 { get; set; }
+    }
+}
