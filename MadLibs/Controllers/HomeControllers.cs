f1c3929f52fe3d1914c3c90c5ea6c48bc1734804
Chloe ONeil <chloesporri@yahoo.com>
2023-02-12 16:32:07 -0800

    add HomeControllers Co-authored-by: Najma Abdi <anajmabdi@gmail.com>

---
+++ MadLibs/Controllers/HomeControllers.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using MadLibs.Models;
+
+namespace Madlibs.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [Route("/form")]
+    public ActionResult Form() { return View(); 
+    }
+
+    [Route("/madlibsform")] 
+    public ActionResult MadLibs(string noun1, string noun2, string noun3, string adjective1, string adjective2, string food1, string food2, string verb, string verbing, string body, string color1, string color2) 
+    { 
+      EntryForm myEntryForm = new EntryForm();
+      myEntryForm.Noun1 = noun1;
+      myEntryForm.Noun2 = noun2;
+      myEntryForm.Noun3 = noun3;
+      myEntryForm.Adjective1 = adjective1;
+      myEntryForm.Adjective2 = adjective2;
+      myEntryForm.Food1 = food1;
+      myEntryForm.Food2 = food2;
+      myEntryForm.Verb = verb;
+      myEntryForm.Verbing = verbing;
+      myEntryForm.Color1 = color1;
+      myEntryForm.Color2 = color2;
+      return View(myEntryForm);
+    }
+  }
+}
